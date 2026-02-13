2d9e742f4f837b0f697cd71c83e4ac2c12b4c772
Michael Kalantar <kalantar@us.ibm.com>
2026-02-13 12:38:32 -0500

    chore: configure Jest for React Native testing

---
+++ jest.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['<rootDir>/tests/ui/setup.js'],
+  testMatch: ['<rootDir>/tests/ui/**/*.test.js'],
+  moduleFileExtensions: ['js', 'jsx', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|expo|@expo|@react-native|react-native-paper|expo-av)/)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  collectCoverageFrom: [
+    'App.js',
+    'components/**/*.{js,jsx}',
+    'utils/**/*.{js,jsx}',
+    '!**/__tests__/**',
+    '!**/node_modules/**',
+  ],
+};
